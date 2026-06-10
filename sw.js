6cbebf9735596d07c425d6a398d5eae6831530f0
apoorva89 <apnonie@gmail.com>
2026-06-10 15:48:37 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = "garden-journal-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "https://cdnjs.cloudflare.com/ajax/libs/react/18.2.0/umd/react.production.min.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/react-dom/18.2.0/umd/react-dom.production.min.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/7.23.2/babel.min.js"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match("/index.html")))
+  );
+});
